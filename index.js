36a52b7f0c27a184e503082fbf3a4184b77c95e6
luisina.muller <luisina.e.muller@gmail.com>
2023-04-14 20:13:35 -0300

    Hice un push con las modificaciones del formulario de inicio de sesión

--- index.js
+++ index.js
@@ -51,3 +51,108 @@ $(document).ready(function() {
   })
 })
 
+
+//VALIDACIÓN DE FORMULARIO DE INGRESO Y DE REGISTRO UTILIZANDO JQUERY
+$(document).ready(function() {
+  $("#primer-form").validate(
+      {
+          rules: {
+              nombre: {
+                  required: true,
+                  minlength: 3,
+              },
+              email: {
+                  required: true,
+                  email: true
+                 
+              },
+              clave: {
+                 required: true,
+                 minlength: 7
+              }
+         },
+         messages: {
+              nombre: {
+                  required: "El nombre de usuario debe contener al menos 3 letras",
+                  minlength: "El nombre de usuario debe contener al menos 3 letras"
+              },
+              email: {
+                  required: "Debe ingresar su correo electrónico",
+                  email: "El email no tiene el formato requerido"
+                  
+              },
+              clave: {
+                  required: "Ingrese su contraseña",
+                  minlength: "La contraseña debe contener al menos 7 caracteres"
+              }
+              
+         }
+      }
+  )
+
+  $("#segundo-form").validate(
+      {
+          rules: {
+              usuarioRegistro: {
+                  required:  true,
+                  minlength: 3,
+              },
+              emailRegistro: {
+                  required: true,
+                  email: true
+                 
+              },
+              claveRegistro: {
+                 required: true,
+                 minlength: 7
+              },
+              claveConfirm: {
+                  required: true,
+                  minlength: 7
+               }
+
+         },
+         messages: {
+              usuarioRegistro: {
+                  required: "El nombre de usuario debe contener al menos 3 letras",
+                  minlength: "El nombre de usuario debe tener al menos 3 letras"
+              },
+              emailRegistro: {
+                  required: "Ingrese su correo electrónico",
+                  email: "El email no tiene el formato requerido"
+              },
+              claveRegistro: {
+                  required: "Ingrese su contraseña",
+                  minlength: "La contraseña debe contener al menos 7 caracteres"
+              },
+              claveConfirm: {
+                  required: "Confirme la contraseña ingresada",
+                  minlength: "La contraseña debe contener al menos 7 caracteres"
+
+              }
+              
+         }
+      }
+  )
+
+});
+
+
+// Código para obtener pdf
+
+function genPDF(){
+  var doc= new jsPDF();
+  let Usuario=document.getElementById("usuarioRegistro").value;
+  let correoElectronico=document.getElementById("emailRegistro").value;
+  let clave=document.getElementById("claveRegistro").value;
+  let ConfirmacionDeClave=document.getElementById("claveConfirm").value;
+
+  doc.text("Usuario: "+ Usuario,10,20);
+  doc.text("Email: "+ correoElectronico,10,30);
+  doc.text("Contraseña: "+ clave,10,40);
+  doc.text("Confirmación de contraseña: "+ConfirmacionDeClave,10,50);
+  doc.save("Midocumento.pdf")
+}
+
+
+
