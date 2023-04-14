0550a12ba90c68c6776b1bb116499e0ab03e4849
RodriPal <rodrigopaliza@gmail.com>
2023-04-14 01:28:04 -0300

    ajax integrado api consumiendo

--- index.js
+++ index.js
@@ -25,3 +25,29 @@ function enviarConsulta(event) {
 }
 
 
+
+
+$(document).ready(function() {
+  $.ajax({
+    method: "GET",
+    url: "https://reqres.in/api/users",
+    dataType: 'json',
+    success: function(card){
+        let crearTarjetas = card
+        console.log(crearTarjetas.data)
+        let reviews = document.querySelector('#reviews')
+        card.data.forEach(usuario => {
+          reviews.innerHTML += ` 
+            <div class="card rounded-3 bg-transparent shadow-none border-0" style="width: 18rem;">
+              <img src="${usuario.avatar}" class="card-img-top rounded-circle img-thumbnail align-self-center " alt="..." style="height: 160px; width: 160px;">
+              <div class="card-body text-center">
+                <p class="card-text">${usuario.first_name}</p>
+                <p class="card-text">${usuario.last_name}</p>
+              </div>
+            </div>
+          `     
+        });      
+      }
+  })
+})
+
